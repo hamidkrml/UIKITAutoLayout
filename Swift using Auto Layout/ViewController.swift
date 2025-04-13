159df48f77779fee69f1fc4d479d89ddb74421eb
hamidkrml <karimlihamid4@gmail.com>
2025-04-13 17:01:53 +0300

    Initial Commit

---
+++ Swift using Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift using Auto Layout
+//
+//  Created by hamid on 13.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
