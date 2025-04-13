c40cbdd5fb2c11a1e748e49c67c11fe9a03a2791
hamidkrml <karimlihamid4@gmail.com>
2025-04-13 17:28:04 +0300

    Add ViewController implementation with protocol and layout setup

--- Swift using Auto Layout/ViewController.swift
+++ Swift using Auto Layout/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
-
+protocol ViewControllerProtocol{
+    func setupView()
+    func makeLabel(withtext text:String)->UILabel
+}
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
 
 }
 
+extension ViewController :ViewControllerProtocol{
+    
+    func setupView() {
+        let upperLeftLabel = makeLabel(withtext: "upperLeft")
+        view.addSubview(upperLeftLabel)
+        
+        
+        NSLayoutConstraint.activate([
+            upperLeftLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 2),
+            upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 2)
+        ])
+    }
+    func makeLabel(withtext text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .orange
+        return label
+    }
+    
+    
+}
+#Preview{
+    ViewController()
+}
